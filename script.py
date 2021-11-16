d283f31aa02b396ae39c6728e6087547d0dfa8de
mimalakhov <94488402+mimalakhov@users.noreply.github.com>
2021-11-17 00:24:40 +0300

    Update script.py

--- script.py
+++ script.py
@@ -3,11 +3,10 @@ import random
 choice=random.randint(0, 5)
 if (choice == 0):
     print("Bang!")
-    num=1
-    while (num<5):
+    num=0
+    while True:
         path="hahaha"+str(num)+".txt"
         open(path, "w")
         num=num+1
-    
 else:
     print("You win")
